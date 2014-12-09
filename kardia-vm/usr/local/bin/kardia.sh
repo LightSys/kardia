9544facc37b0dad901b42bcfddeb279d20f93eaa
Tim Young <timyoungls@sourceforge.net>
2014-12-09 17:04:28 -0600

    Moved the vm-prep code from initkardia.sh into kardia.sh This allows us to service multiple versions of kardia.sh   for centos_6, centos_7, etc

--- kardia-vm/usr/local/bin/kardia.sh
+++ kardia-vm/usr/local/bin/kardia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # kardia.sh - manage the Kardia / Centrallix VM appliance
-# version: 1.0.1
+# version: 1.0.2
 # os: centos_7
 
 # Some housekeeping stuff.  We may be running under a user account, but
@@ -2721,6 +2721,286 @@ function doSystemUpdate
     # Update menu interface?
     if [ "${SEL%%Menus*}" != "$SEL" ]; then
 	AsRoot UpdateMenus
+    fi
+    }
+
+########################
+## Cleanup stuff #################
+## Prep the vm for distribution ##
+##################################
+
+#########
+## YUM ##
+function vm_prep_cleanYum
+{
+	echo "Cleaning YUM"
+	yum clean all
+	echo
+}
+
+#################
+# Etc directory #
+function vm_prep_setupEtc
+{
+	mkdir -p /etc/samba /etc/pam-script
+	files="issue.kardia issue.kardia-init pam.d/system-auth.kardia samba/smb.conf.noshares samba/smb.conf.onerepo samba/smb.conf.userrepo ssh/sshd_config.kardia pam-script/pam_script_passwd pam.d/system-auth.kardia"
+	directory="/usr/local/src/kardia-git/kardia-vm/etc/"
+	echo "Settting Up the ETC directory"
+	for file in $files; do
+	    if [  ! -e "/etc/$file" ]; then
+		echo -n "$file dne "
+		if [ -e "$directory$file" ]; then
+		    echo "copying from $directory"
+		    cp "$directory$file" /etc/$file
+		else
+		    echo "not in $directory.  Left undone"
+		fi
+	    fi
+	done
+	if [ -e /etc/issue.kardia-init ]; then
+	    cat /etc/issue.kardia-init > /etc/issue
+	else
+	    echo "ERROR: issue.kardia-init does not exist!"
+	    echo "Either run kardia.sh or download the kardia git repo"
+	    echo "This VM will not be ready until that file is in place"
+	    exit
+	fi
+	cat /etc/samba/smb.conf.noshares /etc/samba/smb.conf.onerepo > /etc/samba/smb.conf 2> /dev/null
+	[ -f /etc/ssh/sshd_config.kardia ] && cat /etc/ssh/sshd_config.kardia > /etc/ssh/sshd_config
+	if [ -n "`grep PRELINKING=yes /etc/sysconfig/prelink 2>/dev/null`" ]; then
+	    echo Turning off prelinking
+	    sed -ri 's/PRELINKING=yes/PRELINKING=no/' /etc/sysconfig/prelink
+	fi
+	####################
+	# Add control groups
+	echo "Adding control groups"
+	[ -z "`grep kardia_src /etc/group`" ] && groupadd kardia_src
+	[ -z "`grep kardia_ssh /etc/group`" ] && groupadd kardia_ssh
+	[ -z "`grep kardia_root /etc/group`" ] && groupadd kardia_root
+	echo
+
+	####################
+	# Set the hostname
+	echo "Changing the VM Hostname"
+	N_HOST="KardiaVM"
+	echo "$N_HOST" > /etc/hostname
+	/bin/hostname "$N_HOST"
+	echo
+}
+
+###########
+# SELINUX ##################################################
+function vm_prep_setupSelinux
+{
+    #Change the sysconfig file selinux enabled line to "enabled"
+    echo "Ensuring SELINUX is enabled"
+    sed -i -e "s/^SELINUX=.*/SELINUX=enabled/" /etc/sysconfig/selinux
+    # Enable sharing of home directories
+    echo "Enable sharing of home directories in selinux (long process)"
+    setsebool -P samba_enable_home_dirs on
+    echo "Workaround for selinux bug that forbids sharing /usr/local/src"
+    # Workaround for selinux bug that forbids sharing /usr/local/src
+    chcon -t usr_t /usr/local/src
+    echo
+}
+
+###################
+### NETWORKING ##########################
+# Set the network files to correct values
+function vm_prep_cleanNetwork
+{
+	echo "Cleaning out the MAC addresses from the config file"
+	for addrfile in /etc/sysconfig/network-scripts/ifcfg-e*; do
+		echo -n "cleaning up: $addrfile "
+		#remove the MAC address line.  The MAC should be regenerated
+		#when the VM is cloned.  If this is left here, the VM will
+		#not start the interface
+		sed -i -e "/HWADDR/d" $addrfile
+
+		#remove the UUID line.  The UUID is like the MAC in that
+		#it will be changed when cloned.  If this is left here, the VM will
+		#not start the interface
+		sed -i -e "/UUID/d" $addrfile
+
+		#Make sure the interface starts on boot
+		sed -i -e "s/ONBOOT=.*/ONBOOT=yes/" $addrfile
+
+		echo "done"
+	done
+	echo "Cleaning up firewall"
+	for a in `firewall-cmd --list-ser`; do 
+		if [ $a != "ssh" -a $a != "dhcpv6-client" ]; then 
+			echo "  removing service $a"
+			firewall-cmd --remove-service $a
+			firewall-cmd --perm --remove-service $a
+		fi; 
+	done
+	for a in `firewall-cmd --list-port`; do 
+		echo "  removing port $a"
+		firewall-cmd --remove-port $a
+		firewall-cmd --perm --remove-port $a
+	done
+	echo
+}
+
+
+###########
+# SELINUX ##################################################
+#Change the sysconfig file selinux enabled line to "enabled"
+function vm_prep_cleanSelinux
+{
+	echo "Ensuring SELINUX is enabled"
+	sed -i -e "s/^SELINUX=.*/SELINUX=enabled/" /etc/sysconfig/selinux
+	echo
+}
+
+###########
+### SSH #######################################################
+# Remove the server ssh keys.  They will be regenerated on boot
+# also remove the root ssh keys and authorized hosts files
+function vm_prep_cleanSSH
+{
+	echo "Removing ssh keys and ssh info"
+	rm -vf /etc/ssh/*key*
+	rm -rvf /root/.ssh
+	echo
+}
+
+
+###########
+## FILES #########################################
+# Clean up the contents of history and other files
+function vm_prep_cleanFiles
+{
+    cxfiles="/usr/local/sbin/centrallix /usr/local/sbin/cxpasswd /usr/local/etc/centrallix"
+    cxlibs="/usr/local/lib/StPar* /usr/local/centrallix/ /usr/local/libCentrallix*"
+    cxinc="/usr/local/include/cxlib/"
+    cxetc="/etc/init.d/centrallix"
+    kardiash="/usr/local/src/.initialized /usr/local/src/.cx* /usr/local/src/.mysqlaccess"
+    gitfiles="/usr/local/src/cx-git /usr/local/src/kardia-git"
+	echo "Cleaning up Filesystem"
+	echo "  Cleaning up history"
+	echo "kardia.sh" > /root/.bash_history
+	echo "  Cleaning root git info"
+	echo > /root/.gitconfig
+	echo "  Cleaning root cxhistory"
+	echo > /root/.cxhistory
+	echo "  Cleaning root mysql_history"
+	echo > /root/.mysql_history
+	echo "  Cleaning root less history"
+	echo > /root/.lesshst
+	echo "  Cleaning root vim history"
+	echo > /root/.viminfo
+
+	#remove the file that says Kardia has been initialized
+	rm /usr/local/src/.initialized 2> /dev/null
+
+	#remove any entries in the udev rules. This file is auto written
+	tfile=/etc/udev/rules.d/70-persistent-net.rules
+	if [ -e $tfile ]; then
+		echo > $tfile
+	fi
+	echo "  Cleaning up installed Centrallix files"
+	chkconfig centrallix off 2>/dev/null
+	for file in $cxfiles $cxlibx $cxinc $cxetc $kardiash $gitfiles; do
+	    if [ -f "$file" -o -d "$file" ]; then
+		echo "    $file"
+		rm -rf "$file"
+	    fi
+	done
+	echo "Uninstalling old kernel versions"
+	rpm -qa kernel* | grep -v `uname -r` | while read line; do 
+	    echo "Removing package: $line"
+	    rpm -e $line; 
+	done
+	echo "Removing old log files"
+	find /var/log -name *-$(date +%Y)* -type f -print0 | xargs -0 rm -f --
+
+	echo 
+}
+
+######################
+###  Kardia Users  #######################################
+function vm_prep_cleanUsers
+{
+	#remove users
+	echo "Removing any Kardia Users"
+	for USERNAME in $(grep -- '- Kardia:' /etc/passwd | sed 's/^\([^:]*\):[^:]*:\([^:]*\):.*/\1/'); do
+		killall -u $USERNAME
+		echo "  removing Kardia user $USERNAME from linux"
+		userdel -r $USERNAME
+		echo "  removing Kardia user $USERNAME from samba"
+		pdbedit -u $USERNAMR -x
+		echo "  removing mysql user $USERNAME"
+		mysql -e "DROP USER $USERNAME@'localhost';" 2> /dev/null
+		mysql -e "DROP USER $USERNAME@'%';" 2> /dev/null
+	done
+	echo "Dropping Kardia_DB"
+	mysql -e "DROP DATABASE Kardia_DB;" 2>/dev/null
+}
+
+###################
+### Empty Space #######################################
+# Fill the empty space on the disk for easy compressing 
+function vm_prep_cleanEmptySpace
+{
+	echo "Filling remainder of HD with zeroes.  This takes some time"
+	partitions=`df | egrep -v "none|Filesystem|tmpfs" | sed 's/.* //'`
+	for a in $partitions; do
+		echo "partition $a"
+		file="$a/zerofile"
+		if [ -f $file ]; then
+			echo "Oops.  $file already exists. Delete it or something"
+		else
+			echo -n "Wait... filling in $file with zeros "
+			dd if=/dev/zero of=$file bs=1024 2>/dev/null
+			echo -n "done - Deleting $file "
+			rm -f $file
+			echo "deleted"
+		fi
+	done
+	echo "This VM should now compress very nicely"
+}
+
+function doCleanup
+    {
+    if [ $# -eq 0 ]; then
+	#make sure things are copied over from the repo
+	vm_prep_setupEtc
+	#clean out yum cache
+	vm_prep_cleanYum
+	#clean up network settings
+	vm_prep_cleanNetwork
+	#make sure selinux is enabled
+	vm_prep_cleanSelinux
+	#wipe ssh keys
+	vm_prep_cleanSSH
+	#clean up .history files and any extra users
+	vm_prep_cleanFiles
+	#remove Kardia users
+	vm_prep_cleanUsers
+	#zero out empty space so it compresses nicely
+	vm_prep_cleanEmptySpace
+	#
+	echo "This VM is prepped to be rolled out"
+	echo "Ensure the root password is set to what the PDF says it will be"
+    else
+	command="$1"
+	wholecmd="$@"
+	if [ "$command" != "" ]; then
+	    if [ "$(type -t $command)" = "function" ]; then
+		"$wholecmd"
+		exit
+	    elif [ "$(type -t vm_prep_$command)" = "function" ]; then
+		vm_prep_"$wholecmd"
+	    elif [ "$(type -t vm_prep_clean$command)" = "function" ]; then
+		vm_prep_clean"$wholecmd"
+		exit
+	    elif [ "$(type -t vm_prep_setup$command)" = "function" ]; then
+		vm_prep_setup"$wholecmd"
+		exit
+	    fi
+	fi
     fi
     }
 
