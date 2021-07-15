44da6442d62ce53d7d141eca3c28a53343218350
Zachary Allen <zacharys.allen@calbaptist.edu>
2021-07-15 15:34:47 -0600

    My new python script, plus notes

---
+++ kardia-dev-tools/zac_python_script.py
@@ -0,0 +1,43 @@
+import csv
+
+counter = 0
+setID = set()
+setAmounts = set()
+setAmountErrorsId = set()
+setDatesErrorsId = set()
+
+with open('moreData.csv') as csvfile:
+    rowReader = csv.reader(csvfile)
+    prevRow = rowReader.next()
+    for row in rowReader:
+        if (row[2] == prevRow[2]):
+            if(row[20] != prevRow[6]):
+                setDatesErrorsId.add(row[1])
+            if(row[4] == prevRow[4]):
+                setAmountErrorsId.add(row[1])
+            
+        prevRow = row
+
+print "Amount Errors: "
+print setAmountErrorsId
+print "\nDate Errors: "
+print setDatesErrorsId
+    
+#    for row in rowReader:
+#        setID.add(row[1])
+#        setAmounts.add(row[4])
+
+
+#        setID.add(row[1])
+#        setAmounts.add(row[4])
+#        if(row[1] == "\"100400\""):
+#            counter = counter + 1
+#            print row
+
+#print counter
+#print len(setID)
+#print "\"100400\"" in setID
+#print "$4.00" in setAmounts
+
+    #print spamreader.next()[2]
+    #print "\n"
