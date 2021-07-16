53bf3fc386d2e5a9d45cba708b87398245147a58
Zachary Allen <zacharys.allen@calbaptist.edu>
2021-07-16 14:41:44 -0600

    Adjusted the python and added notes

--- kardia-dev-tools/zac_python_script.py
+++ kardia-dev-tools/zac_python_script.py
@@ -1,6 +1,7 @@
 import csv
 
 counter = 0
+modCount = 0
 setID = set()
 setAmounts = set()
 setAmountErrorsId = []
@@ -12,15 +13,19 @@ with open('moreData.csv') as csvfile:
     prevRow = rowReader.next()
     for row in rowReader:
         if (row[2] == prevRow[2] and row[1] == prevRow[1]):
-            if(row[6] != prevRow[20]):
+            if(row[6] != prevRow[20] and row[6] != "" and prevRow[20] != ""):
                 setDatesErrorsPrev.append(row[1])
-                #setDatesErrorsPrev.append(row[2])
-                #setDatesErrorsPrev.append(row[3])
-            if(row[21] != prevRow[5]):
+                setDatesErrorsPrev.append(row[2])
+                setDatesErrorsPrev.append(row[3])
+            if(row[21] != prevRow[5] and row[21] != "" and row[5] != ""):
                 setDatesErrorsNext.append(row[1])
+                setDatesErrorsNext.append(row[2])
+                setDatesErrorsNext.append(row[3])
             if(row[4] == prevRow[4]):
                 setAmountErrorsId.append(row[1])
                 setAmountErrorsId.append(row[2])
+                setAmountErrorsId.append(row[3])
+            
             
         prevRow = row
 
@@ -30,20 +35,38 @@ print "\nDate Errors Prev: "
 print setDatesErrorsPrev
 print "\nDate Errors Next: "
 print setDatesErrorsNext
+print "\n"
 
 f = open("thingsToEdit.txt", "w")
 f.write("Amount Errors: \n")
 for error in setAmountErrorsId:
-    f.write(error)
-    f.write("\n")
+    if(modCount % 3 == 2):
+        f.write(error)
+        f.write("\n")
+    else:
+        f.write(error)
+        f.write(" ")
+    modCount += 1
 f.write("\nDate Errors Prev: \n")
+modCount = 0
 for error in setDatesErrorsPrev:
-    f.write(error)
-    f.write("\n")
+    if(modCount % 3 == 2):
+        f.write(error)
+        f.write("\n")
+    else:
+        f.write(error)
+        f.write(" ")
+    modCount += 1
 f.write("\nDate Errors Next: \n")
+modCount = 0
 for error in setDatesErrorsNext:
-    f.write(error)
-    f.write("\n")
+    if(modCount % 3 == 2):
+        f.write(error)
+        f.write("\n")
+    else:
+        f.write(error)
+        f.write(" ")
+    modCount += 1
 f.close()
 
 #    for row in rowReader:
