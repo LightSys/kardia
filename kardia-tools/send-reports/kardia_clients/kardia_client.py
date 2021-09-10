9aab980c0cd52a02e95c81bf7dee5f81567713b3
sheesania <sheesania@users.noreply.github.com>
2021-09-10 10:23:43 -0600

    Kardia Rest API client can grab scheduled report info + misc refactoring

--- kardia-tools/send-reports/kardia_clients/kardia_client.py
+++ kardia-tools/send-reports/kardia_clients/kardia_client.py
@@ -4,18 +4,10 @@ from enum import Enum
 from typing import Dict, List
 
 
-class ScheduledReportDeliveryMethod(Enum):
-    EMAIL = "E"
-    WEB = "W"
-    PRINT = "P"
-
-
 class ScheduledReport:
-    def __init__(self, report_file: str, delivery_method: ScheduledReportDeliveryMethod, year: int, month: int,
-            day: int, hour: int, minute: int, second: int, recipient_name: str, recipient_emails: List[str],
-            params: Dict[str, str]):
+    def __init__(self, report_file: str, year: int, month: int, day: int, hour: int, minute: int, second: int,
+            recipient_name: str, recipient_emails: List[str], params: Dict[str, str]):
         self.report_file = report_file
-        self.delivery_method = delivery_method
         self.send_date = datetime(year, month, day, hour=hour, minute=minute, second=second)
         self.recipient_name = recipient_name
         self.recipient_emails = recipient_emails
@@ -23,9 +15,9 @@ class ScheduledReport:
 
     
     def __repr__(self) -> str:
-        return (f'report_file: {self.report_file}\ndelivery_method: {self.delivery_method}\n' +
+        return (f'report_file: {self.report_file}\n' +
             f'send_date: {str(self.send_date)}\nrecipient_name: {self.recipient_name}\n' +
-            f'recipient_emails: {str(self.recipient_emails)}\nparams: {str(self.params)}')
+            f'recipient_emails: {str(self.recipient_emails)}\nparams: {str(self.params)}\n')
 
 
 class KardiaClient(abc.ABC):
--- kardia-tools/send-reports/kardia_clients/rest_api_kardia_client.py
+++ kardia-tools/send-reports/kardia_clients/rest_api_kardia_client.py
@@ -1,5 +1,6 @@
 from kardia_api import Kardia
-from kardia_clients.kardia_client import KardiaClient, ScheduledReport, ScheduledReportDeliveryMethod
+from kardia_clients.kardia_client import KardiaClient, ScheduledReport
+from typing import Dict, List
 
 
 class RestAPIKardiaClient(KardiaClient):
@@ -8,35 +9,65 @@ class RestAPIKardiaClient(KardiaClient):
     def __init__(self, kardia_url, user, pw):
         self.kardia = Kardia(kardia_url, user, pw)
 
+
+    def _get_params_for_sched_report(self, schedReportId: str) -> Dict[str, str]:
+        params = {}
+        self.kardia.report.setParams(res_attrs="basic")
+        # TODO: handle sad response
+        paramsJson = self.kardia.report.getSchedReportParams(schedReportId).json()
+        for paramName, paramInfo in paramsJson.items():
+            if paramName.startswith("@id"):
+                continue
+            params[paramName] = paramInfo["value"]
+        return params
+
+
+    def _get_emails_for_partner(self, partnerId: str) -> List[str]:
+        recipientEmails = []
+        self.kardia.partner.setParams(res_attrs="basic")
+        # TODO: handle sad response
+        partnerContactsJson = self.kardia.partner.getPartnerContactInfos(partnerId).json()
+        for contactId, contactInfo in partnerContactsJson.items():
+            if contactId.startswith("@id"):
+                continue
+            if contactInfo["contact_type"] != "Email":
+                continue
+            recipientEmails.append(contactInfo["contact"])
+        return recipientEmails
+
     
     def get_scheduled_reports_to_be_sent(self):
         self.kardia.report.setParams(res_attrs="basic")
         # TODO: handle sad response
         schedReportJson = self.kardia.report.getSchedReportsToBeSent().json()
-        for schedReportAttribute, schedReportValue in schedReportJson.items():
-            if schedReportAttribute.startswith("@id"):
+        schedReports = []
+
+        for schedReportId, schedReportInfo in schedReportJson.items():
+            if schedReportId.startswith("@id"):
                 continue
-            report_file = schedReportValue["report_file"]
-            delivery_method = ScheduledReportDeliveryMethod(schedReportValue["delivery_method"])
-            year = schedReportValue["send_date"]["year"]
-            month = schedReportValue["send_date"]["month"]
-            day = schedReportValue["send_date"]["day"]
-            hour = schedReportValue["send_date"]["hour"]
-            minute = schedReportValue["send_date"]["minute"]
-            second = schedReportValue["send_date"]["second"]
+            if schedReportInfo["delivery_method"] != "E":
+                continue
+            reportFile = schedReportInfo["report_file"]
+            year = schedReportInfo["send_date"]["year"]
+            month = schedReportInfo["send_date"]["month"]
+            day = schedReportInfo["send_date"]["day"]
+            hour = schedReportInfo["send_date"]["hour"]
+            minute = schedReportInfo["send_date"]["minute"]
+            second = schedReportInfo["send_date"]["second"]
 
-            params = {}
-            self.kardia.report.setParams(res_attrs="basic")
             # TODO: handle sad response
-            paramsJson = self.kardia.report.getSchedReportParams(schedReportAttribute).json()
-            for paramAttribute, paramValue in paramsJson.items():
-                if paramAttribute.startswith("@id"):
-                    continue
-                params[paramAttribute] = paramValue["value"]
+            partnerJson = self.kardia.partner.getPartner(schedReportInfo["recipient_partner_key"]).json()
+            recipientName = partnerJson["partner_name"]
 
-            schedReport = ScheduledReport(report_file, delivery_method, year, month, day, hour, minute, second, "bewy",
-                ["bewy@heemin.com"], params)
+            recipientEmails = self._get_emails_for_partner(schedReportInfo["recipient_partner_key"])
+            params = self._get_params_for_sched_report(schedReportId)
+
+            schedReport = ScheduledReport(reportFile, year, month, day, hour, minute, second, recipientName,
+                recipientEmails, params)
             print(schedReport)
+            schedReports.append(schedReport)
+        
+        return schedReports
 
 
     def call_report(self, report_file, params, generated_file_dir):
