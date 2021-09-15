11da91d0fc240283d6464dc0c8898823e20aa9eb
sheesania <sheesania@users.noreply.github.com>
2021-09-15 10:54:44 -0600

    Attach pdf reports

--- kardia-tools/send-reports/senders/email_report_sender.py
+++ kardia-tools/send-reports/senders/email_report_sender.py
@@ -1,4 +1,6 @@
 import email
+import email.policy
+import os
 import smtplib
 from datetime import datetime
 from senders.sender import ReportSender, SendingInfo, SentStatus
@@ -17,13 +19,21 @@ class EmailReportSender(ReportSender):
     def send_report(self, report_path, contact_infos: List[str], replaceable_params):
         smtp = smtplib.SMTP("localhost")
         for contact_info in contact_infos:
+
+            # Gather replaceable parameters and replace them in the template
             replaceable_params_for_contact = {**replaceable_params}
             replaceable_params_for_contact["email"] = contact_info
             self._add_replaceable_params(replaceable_params_for_contact)
             email_for_contact = self.email_template
             for param_name, param_value in replaceable_params_for_contact.items():
                 email_for_contact = email_for_contact.replace(f'{{{{{param_name}}}}}', param_value)
-            msg = email.message_from_string(email_for_contact)
+
+            # Create an email message and attach the report file
+            msg = email.message_from_string(email_for_contact, policy=email.policy.EmailPolicy())
+            (_, filename) = os.path.split(report_path)
+            with open(report_path, 'rb') as fp:
+                msg.add_attachment(fp.read(), maintype="application", subtype="pdf", filename=filename)
+
             try:
                 smtp.send_message(msg)
             except smtplib.SMTPResponseException as se:
