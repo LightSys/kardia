58819c5ba196eaa520c372739283e6d0ddcc59d5
Israel <israel.fuller@lightsys.org>
2025-11-14 11:48:53 -0700

    Add ClusterDriverRequirements (forgot to commit them before). Add known issues to string similarity documentation. Clean up and organize todos. Clean up testing code in several files.

--- kardia-scripts/cron/update_duplicates.sh
+++ kardia-scripts/cron/update_duplicates.sh
@@ -14,5 +14,5 @@
 # Make sure /usr/local binaries are in the $PATH
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin
 
+# TODO: Greg - We need to update this cron to call the new file.
 /usr/local/bin/test_obj -c /usr/local/etc/centrallix.conf -u kardia -p $(cat /usr/local/etc/centrallix/kardia-auth) -q -C 'ls /apps/kardia/modules/base/update_duplicates.qy' 2>/dev/null >/dev/null
-
